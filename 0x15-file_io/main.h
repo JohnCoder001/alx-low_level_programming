9fb36c9419fdc3019bb440bf72e70b150f4905a1
JohnCoder001 <112697811+JohnCoder001@users.noreply.github.com>
2023-04-10 22:04:19 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 /*
- * File: holberton.h
- * Auth: Godswill Kalu
+ * File: Main.h
+ * Auth: Johncoder001
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
